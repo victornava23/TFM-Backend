b718fe080ca267153b267a5d9678d98835281966
victornava23 <victornava23@gmail.com>
2023-05-03 12:31:53 -0600

    se cambia configuración de BD y se modifica script start.sh

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const dbsettings = {
     user: 'admin',
     password: 'M4st3r2023',
-    server: 'tfm-db.cj3dc19dah6y.us-east-1.rds.amazonaws.com',
+    server: 'tfm-db.cjchwy4m83lf.eu-west-1.rds.amazonaws.com',
     database: 'acuamattic_tfm_db',
     
     options: {
